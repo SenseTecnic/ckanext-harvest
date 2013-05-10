018352518c84c900c673656d01a263dffeea5bb3
Mark Duppenthaler <mduppes@gmail.com>
2013-05-10 10:43:34 -0700

    Remove extra file

--- ckanext/harvest/harvesters/__init__.py
+++ ckanext/harvest/harvesters/__init__.py
@@ -1,6 +1,5 @@
 from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
 from london import DataLondonGovUkHarvester
-from digitaliser_dk import DigitaliserDkHarvester
 
 from ckanext.harvest.harvesters.base import HarvesterBase
 
