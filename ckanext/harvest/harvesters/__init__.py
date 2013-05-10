e53dca1a70084bc11533f5551e23e85b1997d7fc
Mark Duppenthaler <mduppes@gmail.com>
2013-05-10 10:38:09 -0700

    Update extension to include plugin to pull from a london data site that was copied over from https://github.com/okfn/ckanext-pdeu/tree/master/ckanext/pdeu/harvesters.

--- ckanext/harvest/harvesters/__init__.py
+++ ckanext/harvest/harvesters/__init__.py
@@ -1,3 +1,6 @@
-
 from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
+from london import DataLondonGovUkHarvester
+from digitaliser_dk import DigitaliserDkHarvester
+
 from ckanext.harvest.harvesters.base import HarvesterBase
+
--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
 	# Add plugins here, eg
 	harvest=ckanext.harvest.plugin:Harvest
 	ckan_harvester=ckanext.harvest.harvesters:CKANHarvester
+	london_harvester=ckanext.harvest.harvesters:DataLondonGovUkHarvester
 	[paste.paster_command]
 	harvester = ckanext.harvest.commands.harvester:Harvester
 	""",
